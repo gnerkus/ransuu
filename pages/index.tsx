ff6c959f8f9cfa9b7ec0fc8500621f2edfb313fa
gnerkus <ifeanyioraelosi@gmail.com>
2023-06-02 10:17:09 +0200

    remove unused CSS declarations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import styles from "@/styles/Home.module.css";
 import SVGNodeEditor from "@/components/SVGNodeEditor";
 
 export default function Home() {
@@ -11,7 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main className="flex gap-1 items-center p-24 h-screen w-screen">
         <SVGNodeEditor />
       </main>
     </>
